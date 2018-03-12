590146ebf042da7b72415885714c7f85e32fe5ab
Michelle Oliveira <miolivc@outlook.com>
2018-03-12 15:06:18 -0300

    add porcentagem no perfil

--- client/www/js/controllers/perfilFuncionarioCtrl.js
+++ client/www/js/controllers/perfilFuncionarioCtrl.js
@@ -5,7 +5,9 @@ app.controller('perfilFuncionarioCtrl', function($scope, $state, ionicMaterialIn
     $scope.funcionario = {
     	"id": 7,
         "name":"Maria Silva",
-        "admissao": "2013-02-14"
+        "admissao": "2013-02-14",
+        "positivo": "50",
+        "negativo": "50"
     };
 
     $scope.addIncidente = function(idFunc) {
