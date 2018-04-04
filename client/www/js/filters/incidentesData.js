576e91d3a10c64c23388d22ded011401977fe4d3
Michelle Oliveira <miolivc@outlook.com>
2018-04-04 15:47:19 -0300

    solve filter data

--- client/www/js/filters/incidentesData.js
+++ client/www/js/filters/incidentesData.js
@@ -1,20 +1,30 @@
 
+function format(data) {
+
+	// let day = data.substring(0,2);
+	// let month = data.substring(3,2);
+	// let year = data.substring(7,2);
+
+	return new Date(data).getTime();
+}
+
 app.filter("incidentesData", function() {
 	return function(lista, dataInicio, dataFim) {
-
-		if (dataInicio == undefined || dataFim == undefined) {
+		// console.log(new Date(dataInicio).getTime(), new Date(dataFim).getTime());
+		if (! angular.isDefined(dataInicio) || ! angular.isDefined(dataFim)) {
+			
 			return lista;
 		}
-
-		dataInicioF = new Date(dataInicio).toLocaleString();
-		dataFimF = new Date(dataFim).toLocaleString();
-
 		let lista_retorno = [];
-		for (let i = 0; i > lista.lenght; i++) {
-			if (lista[i].data >= dataInicioF && lista[i].data <= dataFimF) {
+
+		for (let i = 0; i < lista.length; i++) {
+			let data = format(lista[i].data);
+			
+			if (data >= dataInicio.getTime() && data <= dataFim.getTime()) {
 				lista_retorno.push(lista[i]);
 			}
 		}
+
 
 		return lista_retorno;
 	}
